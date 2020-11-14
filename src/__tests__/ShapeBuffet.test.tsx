efbc7b37e758635061a2914c054b42b7a1c9356e
phembarl <phembarl@yahoo.com>
2020-11-14 15:07:26 +0100

    update tests

--- src/__tests__/ShapeBuffet.test.tsx
+++ src/__tests__/ShapeBuffet.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ShapeForm from '../components/ShapeForm';
 import ShapeBuffet from '../components/ShapeBuffet';
@@ -8,15 +8,17 @@ const props = {
   createShape: () => {},
 };
 
-afterEach(() => {
-  cleanup();
-});
-
 test('renders component successfully', () => {
   const app = render(<ShapeBuffet />);
 
   expect(app).toBeDefined();
   expect(<ShapeForm {...props} />).toBeDefined();
+});
+
+test('disables clear button on initial render', () => {
+  render(<ShapeBuffet />);
+  const clear = screen.getByText('Clear');
+  expect(clear).toBeDisabled();
 });
 
 test('creates selected shape', () => {
@@ -28,4 +30,43 @@ test('creates selected shape', () => {
   userEvent.click(submit);
 
   expect(shapeDiv).toHaveClass('square');
+});
+
+test('Toggles color picker', () => {
+  render(<ShapeBuffet />);
+  const submit = screen.getByText('Create Shape');
+  const colorBtn = screen.getByText('Change Color');
+  userEvent.selectOptions(screen.getByTestId('shape-select'), 'square');
+  userEvent.type(screen.getByPlaceholderText('Length'), '50');
+  userEvent.click(submit);
+  userEvent.click(colorBtn);
+  screen.getByTestId('color-picker');
+
+  const HideColorBtn = screen.getByText('Hide Color Picker');
+  userEvent.click(HideColorBtn);
+  expect(screen.queryByTestId('color-picker')).toBeNull();
+});
+
+test('clears shape', () => {
+  render(<ShapeBuffet />);
+  const shapeDiv = screen.getByTestId('shape-div');
+  const submit = screen.getByText('Create Shape');
+  const clear = screen.getByText('Clear');
+  userEvent.selectOptions(screen.getByTestId('shape-select'), 'square');
+  userEvent.type(screen.getByPlaceholderText('Length'), '50');
+  userEvent.click(submit);
+
+  expect(shapeDiv).toHaveClass('square');
+  userEvent.click(clear);
+  expect(clear).toBeDisabled();
+  expect(shapeDiv).not.toHaveClass('square');
+});
+
+test('Updates History list', () => {
+  render(<ShapeBuffet />);
+  const submit = screen.getByText('Create Shape');
+  userEvent.selectOptions(screen.getByTestId('shape-select'), 'square');
+  userEvent.type(screen.getByPlaceholderText('Length'), '50');
+  userEvent.click(submit);
+  screen.getByText('square - 50');
 });
