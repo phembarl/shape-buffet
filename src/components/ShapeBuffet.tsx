9cc2dd24e16ff1836ecbbd24d3c1b55ac178ddeb
phembarl <phembarl@yahoo.com>
2020-11-14 14:09:52 +0100

    implement shape history

--- src/components/ShapeBuffet.tsx
+++ src/components/ShapeBuffet.tsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { shapeStyle } from '../helpers';
 import ShapeForm from './ShapeForm';
 import ColorPicker from 'react-pick-color';
+import { v4 as uuid4 } from 'uuid';
 
 interface ShapeState {
   name: string;
   length: string;
   radius: string;
-  color?: string;
 }
 
 const ShapeBuffet = () => {
@@ -26,6 +26,9 @@ const ShapeBuffet = () => {
   const [pickerColor, setPickerColor] = useState(shape.color || '#ff6363');
 
   const [showPicker, setShowPicker] = useState(false);
+  const [prevShapes, setPrevShapes] = useState<any[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [history, setHistory] = useState('history');
 
   useEffect(() => {
     localStorage.shape = JSON.stringify(shape);
@@ -39,15 +42,26 @@ const ShapeBuffet = () => {
   const createShape = (shapeDetails: ShapeState) => {
     const { name, length, radius } = shapeDetails;
     setShape({ ...shape, name, length, radius });
+    setPrevShapes([
+      ...prevShapes,
+      { name, length, radius, color: pickerColor, id: uuid4() },
+    ]);
   };
 
   const clearShape = () => {
     setShape(initalState);
+    setPickerColor('#ff6363');
     setShowPicker(false);
   };
 
   const setColor = (color: string) => {
     setShape({ ...shape, color });
+  };
+
+  const handleHistory = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    const histShape = prevShapes.find(prevShape => prevShape.id === value);
+    setShape(histShape);
   };
 
   const chosenShapeStyle = shapeStyle(shape);
@@ -71,6 +85,17 @@ const ShapeBuffet = () => {
         >
           Clear
         </button>
+
+        <select className="history" value={history} onChange={handleHistory}>
+          <option value="history" disabled>
+            History
+          </option>
+          {prevShapes.map(sh => (
+            <option value={sh.id}>
+              {sh.name} - {sh.length || sh.radius}
+            </option>
+          ))}
+        </select>
       </div>
 
       <div className="container">
@@ -87,7 +112,7 @@ const ShapeBuffet = () => {
             className={`shape ${shape.name}`}
             style={{ ...chosenShapeStyle, backgroundColor: shape.color }}
             data-testid="shape-div"
-          ></div>
+          />
           <h3 className="shape-name">{shape.name}</h3>
         </div>
       </div>
